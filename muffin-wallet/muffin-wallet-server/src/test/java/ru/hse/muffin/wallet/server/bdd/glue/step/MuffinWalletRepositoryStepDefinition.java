7a1bdbd1da9efa5c428533d99b3e0c5d981f78fe
Serkan Dereli <78605109+SarcasticNickname@users.noreply.github.com>
2026-02-03 20:10:00 +0000

    Added observe

--- muffin-wallet/muffin-wallet-server/src/test/java/ru/hse/muffin/wallet/server/bdd/glue/step/MuffinWalletRepositoryStepDefinition.java
+++ muffin-wallet/muffin-wallet-server/src/test/java/ru/hse/muffin/wallet/server/bdd/glue/step/MuffinWalletRepositoryStepDefinition.java
@@ -1,5 +1,6 @@
 package ru.hse.muffin.wallet.server.bdd.glue.step;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 import io.cucumber.java.en.Given;
@@ -15,26 +16,30 @@ import ru.hse.muffin.wallet.data.api.dto.MuffinWallet;
 @AllArgsConstructor
 public class MuffinWalletRepositoryStepDefinition {
 
-  private final MuffinWalletRepository muffinWalletRepository;
+    private final MuffinWalletRepository muffinWalletRepository;
 
-  @Given("muffin wallet exists with parameters:")
-  public void muffinWalletExistsWithParams(Map<String, String> params) {
-    var muffinWallet = new MuffinWallet();
+    @Given("muffin wallet exists with parameters:")
+    public void muffinWalletExistsWithParams(Map<String, String> params) {
+        var muffinWallet = new MuffinWallet();
 
-    muffinWallet.setId(UUID.fromString(params.get("id")));
-    muffinWallet.setBalance(new BigDecimal(params.get("balance")));
-    muffinWallet.setOwnerName(params.get("owner_name"));
+        muffinWallet.setId(UUID.fromString(params.get("id")));
+        muffinWallet.setBalance(new BigDecimal(params.get("balance")));
+        muffinWallet.setOwnerName(params.get("owner_name"));
 
-    muffinWalletRepository.save(muffinWallet);
-  }
+        muffinWalletRepository.save(muffinWallet);
+    }
 
-  @Then("check muffin wallet exists with parameters:")
-  public void checkMuffinWalletExistsWithParams(Map<String, String> params) {
-    var allMuffinWallet = muffinWalletRepository.findAll(Pageable.ofSize(Integer.MAX_VALUE));
+    @Then("check muffin wallet exists with parameters:")
+    public void checkMuffinWalletExistsWithParams(Map<String, String> params) {
+        var allMuffinWallet = muffinWalletRepository.findAll(Pageable.ofSize(Integer.MAX_VALUE));
 
-    assertNotNull(allMuffinWallet.filter((m) ->
-        m.getBalance().equals(new BigDecimal(params.get("balance")))
-            && m.getOwnerName().equals(params.get("owner_name"))
-    ).toList().getFirst());
-  }
+        var matches = allMuffinWallet.filter(m ->
+                m.getBalance().equals(new BigDecimal(params.get("balance")))
+                        && m.getOwnerName().equals(params.get("owner_name"))
+        ).toList();
+
+        assertNotNull(matches);
+        assertFalse(matches.isEmpty());
+        assertNotNull(matches.get(0));
+    }
 }
